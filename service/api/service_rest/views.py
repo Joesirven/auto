fcafb9bf524708550d408d42b10652b051bd43e8
Jose <jose@oldcutler.club>
2023-07-26 14:27:01 -0400

    IntegrityError exception for Technicians without unique employee id

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
+from django.db import IntegrityError
 
 from .encoders import (
     TechnicianEncoder,
@@ -47,15 +48,23 @@ def api_technicians(request):
             encoder=TechnicianEncoder,
         )
 
-    else:
+    else: #POST
         try:
             content = json.loads(request.body)
+            print(content)
             technician = Technician.objects.create(**content)
             return JsonResponse(
                 technician,
                 encoder=TechnicianEncoder,
                 safe=False,
             )
+        except IntegrityError:
+            response = JsonResponse(
+                {"message": "Employee id already exists. Select a unique employee id."}
+            )
+            response.status_code = 400
+            return response
+
         except AttributeError:
             response = JsonResponse(
                 {"message": "Could not create the technician"}

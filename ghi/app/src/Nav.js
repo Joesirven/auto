d13cd3b63854722727c6c8ed1a1c891550e6d29c
Jose <jose@oldcutler.club>
2023-07-28 09:16:47 -0400

    Inventory API react components final:

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,7 +9,32 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/" end>Home</NavLink>
+            </li>
+            {/* <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Service Appointments
+              </a>
+              <ul className="dropdown-menu">
+                <li><NavLink className="dropdown-item" to="/shoes" end>View Shoes</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/shoes/new" end>Add Shoes</NavLink></li>
+              </ul>
+            </li> */}
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Inventory
+                </a>
+                <ul className="dropdown-menu">
+                    <li><NavLink className="dropdown-item" to="/manufacturers" end>View Manufacturers</NavLink></li>
+                    <li><NavLink className="dropdown-item" to="/manufacturers/new" end>Add Manufacturer</NavLink></li>
+                    <li><NavLink className="dropdown-item" to="/automobiles" end>View Automobiles</NavLink></li>
+                    <li><NavLink className="dropdown-item" to="/automobiles/new" end>Add Automobiles</NavLink></li>
+                    <li><NavLink className="dropdown-item" to="/vehicles" end>View Models</NavLink></li>
+                    <li><NavLink className="dropdown-item" to="/vehicles/new" end>Add Models</NavLink></li>
+                </ul>
+            </li>
           </ul>
         </div>
       </div>
